358a4abf3e7b27e2038f27f386af11125de11edf
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-09-26 17:49:51 +0900

    feat: HomeController.java 메인화면 컨트롤러 추가

---
+++ quiz/quiz/src/main/java/com/example/quiz/Controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.quiz.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController {
+
+
+    //메인 화면 변경
+    @GetMapping("/")
+    public String home() {
+        return "quiz";
+    }
+
+    //대분류 카테고리 접속
+    @GetMapping("/main/{big}")
+    public String BigCategory(@PathVariable Long big) {
+        return null;
+    }
+
+    //소분류 카테고리 접속
+    @GetMapping("/main/{big}/{small}")
+    public String smallCategory(@PathVariable Long big, @PathVariable Long small) {
+        return null;
+    }
+}
